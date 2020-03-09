12c5c5375c3fdb67709e251ccc1fad21bd6719a2
baadamczyk <bartlomiej.adamczyk92@gmail.com>
2020-03-09 13:23:51 +0100

    Initial commit

---
+++ src/test/java/pl/baadamczyk/motosoup/MotoSoupApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.baadamczyk.motosoup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoSoupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
