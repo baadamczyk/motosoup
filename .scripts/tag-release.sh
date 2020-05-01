3fdb3e2cbdf0a6feb01f9fdda06edb0e3d7041f0
baadamczyk <bartlomiej.adamczyk92@gmail.com>
2020-05-01 23:39:53 +0200

    fixup! Set up CD flow for application

--- .scripts/tag-release.sh
+++ .scripts/tag-release.sh
@@ -2,18 +2,10 @@
 
 sh .scripts/prepare-git.sh
 
-ls -a $PACKAGEDIR
 echo ">>> Tagging new release"
-
 export CURRENT_COMMIT=$(git rev-parse --short $TRAVIS_COMMIT)
 
-# TODO: Remove this
-echo ">>> CURRENT COMMIT: $CURRENT_COMMIT"
-
-# git tag $APPLICATION_NAME-$APPLICATION_VERSION $CURRENT_COMMIT
-# SET MESSAGE!
-git tag $APPLICATION_NAME-$APPLICATION_VERSION
-# git tag -af $APPLICATION_NAME-$APPLICATION_VERSION $CURRENT_COMMIT -m "Deployed release: $APPLICATION_VERSION [ci skip]"
+git tag $APPLICATION_NAME-$APPLICATION_VERSION -m "Deployed new stable release"
 
 echo ">> PUSHING TAGS"
 git push origin --tags
