394dffb5d3ab50095cf22200dc00046b34cf56ad
baadamczyk <bartlomiej.adamczyk92@gmail.com>
2020-05-01 20:19:27 +0200

    fixup! Set up CD flow for application

--- .scripts/version-number-upgrade.sh
+++ .scripts/version-number-upgrade.sh
@@ -14,7 +14,7 @@ if [ $TRAVIS_PULL_REQUEST = "false" ]; then
 
   #Set new version in application properties
   newAppVersion=$APPLICATION_VERSION
-  sed -i "s/\(motosoup\.version-number=\).*\$/\1${newAppVersion}/" src/main/resources/application.properties
+  sed -i "s/\(motosoup\.version.number=\).*\$/\1${newAppVersion}/" src/main/resources/application.properties
   cat src/main/resources/application.properties
 
   echo ">>> Pushing changes back to repository..."
